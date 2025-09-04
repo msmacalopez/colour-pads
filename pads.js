a7768b1b36c7558403627c92d5abe14c6b96d683
msmacalopez <msmacalopezs@gmail.com>
2025-09-04 22:40:05 +1000

    first commit

---
+++ pads.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    color: "F18D8B",
+    on: true,
+  },
+  {
+    id: 2,
+    color: "F18D8B",
+    on: true,
+  },
+  {
+    id: 3,
+    color: "F18D8B",
+    on: true,
+  },
+  {
+    id: 4,
+    color: "F18D8B",
+    on: true,
+  },
+];
