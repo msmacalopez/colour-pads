82db7e1b15611def676fc2d5ec4df736cd8785e6
msmacalopez <msmacalopezs@gmail.com>
2025-09-05 19:14:27 +1000

    better sln and shutdown btn

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,44 @@ function App({ darkMode }) {
   //   backgroundColor: darkMode ? "#222222" : "#cccccc",
   // };
 
+  //3)Create toogle fx for padsArray State
+  //this fx is call only from Pad.jsx
+  function toogleOn(id) {
+    //map over the pads array and find item with the id to flip
+    setPadsArray((prevArray) =>
+      prevArray.map((item) => {
+        return item.id === id ? { ...item, on: !item.on } : item;
+      })
+    );
+  }
+
+  //4)All OFF
+  function shutdown() {
+    setPadsArray((prevArray) =>
+      prevArray.map((item) => {
+        return { ...item, on: false };
+      })
+    );
+  }
+
   //2)Map the State array to pass in jsx
   const buttonElements = padsArray.map((pad) => {
-    return <Pad key={pad.id} color={pad.color} on={pad.on} />;
+    return (
+      <Pad
+        key={pad.id}
+        id={pad.id}
+        color={pad.color}
+        on={pad.on}
+        toogleOn={toogleOn}
+      />
+    );
     //return <button style={stylesss} ></button>;
   });
 
   return (
     <main>
-      <div className="pad-container">{buttonElements}</div>
+      <div className="pad-container"> {buttonElements} </div>
+      <button className="red" onClick={shutdown}></button>
     </main>
   );
 }
--- src/Pad.jsx
+++ src/Pad.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Pad = (props) => {
   console.log(props.on);
   //1)initial state for the ON state
-  const [onState, setOnState] = React.useState(props.on);
+  // const [onState, setOnState] = React.useState(props.on);
 
   //2) function to toggle state variable
-  function toggleOn() {
-    setOnState((prevOn) => !prevOn);
-  }
+  // function toggleOn() {
+  //   setOnState((prevOn) => !prevOn);
+  // }
 
   //3)-->add on Click to button
 
@@ -16,8 +16,10 @@ const Pad = (props) => {
     <button
       style={{ background: props.color }}
       // className={props.on ? "on" : null}
-      className={onState ? "on" : null}
-      onClick={toggleOn}
+      className={props.on ? "on" : null}
+      onClick={() => {
+        props.toogleOn(props.id);
+      }}
     ></button>
   );
 };
