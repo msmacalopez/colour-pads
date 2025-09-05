7cd8d1cc966a64df242c5fff0a40eaf0048897d7
msmacalopez <msmacalopezs@gmail.com>
2025-09-05 17:48:20 +1000

    use of derivated state in PADJSX

--- src/Pad.jsx
+++ src/Pad.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 const Pad = (props) => {
   console.log(props.on);
+  //1)initial state for the ON state
+  const [onState, setOnState] = React.useState(props.on);
+
+  //2) function to toggle state variable
+  function toggleOn() {
+    setOnState((prevOn) => !prevOn);
+  }
+
+  //3)-->add on Click to button
+
   return (
     <button
       style={{ background: props.color }}
-      className={props.on ? "on" : null}
+      // className={props.on ? "on" : null}
+      className={onState ? "on" : null}
+      onClick={toggleOn}
     ></button>
   );
 };
